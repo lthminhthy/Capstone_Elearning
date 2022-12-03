8b8fac0ec3556a61cc09ea3156412116298e2251
lthminhthy <lthminhthy@gmail.com>
2022-12-03 23:15:08 +0700

    doneee

--- src/pages/Admin/AdminQuanLyKhoaHoc/AdminThemKhoaHoc.jsx
+++ src/pages/Admin/AdminQuanLyKhoaHoc/AdminThemKhoaHoc.jsx
@@ -27,10 +27,10 @@ const AdminThemKhoaHoc = () => {
     const [tenKH, setTenKH] = useState()
     const [file, setFile] = useState()
     const { thongTinTaiKhoanDefault } = useSelector(state => state.QuanLyNguoiDungReducer)
-    // console.log("thongTinTaiKhoanDefault: ", thongTinTaiKhoanDefault);
 
     const { danhMucKhoaHoc } = useSelector(state => state.QuanLyKhoaHocReducer)
     const { danhsachUser } = useSelector(state => state.QuanLyNguoiDungReducer)
+    // console.log("anhsachUser: ", danhsachUser);
     const [imgSrc, setImgSrc] = useState('');
 
 
@@ -66,22 +66,7 @@ const AdminThemKhoaHoc = () => {
             taiKhoanNguoiTao: '',
 
         },
-        validationSchema: Yup.object({
-            taiKhoan: Yup.string()
-                .min(3, "Mininum 3 characters")
-                .required("Vui lòng nhập tài khoản!"),
-            matKhau: Yup.string()
-                .min(4, "Mininum 4 characters")
-                .required("Vui lòng nhập mật khẩu!"),
-            email: Yup.string()
-                .required("Vui lòng nhập email!"),
-            soDT: Yup.string()
-                .required("Vui lòng nhập số điện thoại!"),
-            hoTen: Yup.string()
-                .required("Vui lòng nhập họ tên!"),
-            maLoaiNguoiDung: Yup.string()
-                .required("Chọn loại người dùng!"),
-        }),
+      
 
         onSubmit: (values) => {
             console.log("valuesubmit: ", values);
@@ -92,11 +77,14 @@ const AdminThemKhoaHoc = () => {
                 frm.append('tenKhoaHoc', tenKH);
                 quanLyKhoaHocSerVice.themKhoaHocUploadHinh(frm).then((result) => {
                     console.log("resultUpload: ", result);
+                    alert('Success!')
+                    navigate(-1)
                 }).catch((error) => {
                     console.log("error: ", error);
                 })
+                
              })
-         
+        
         }
     });
     const handleChangeFile = (e) => {
@@ -146,11 +134,6 @@ const AdminThemKhoaHoc = () => {
         formik.setFieldValue('tenKhoaHoc', values.target.value)
     }
 
-
-
-
-
-
     const onChangeDate = (values) => {
         formik.setFieldValue('ngayTao', moment(values).format('DD/MM/YYYY'))
         console.log("ngayTao: ", moment(values).format('DD/MM/YYYY'));
@@ -160,8 +143,6 @@ const AdminThemKhoaHoc = () => {
         formik.setFieldValue('ngayTao', moment(values).format('DD/MM/YYYY'))
         console.log("ngayTao: ", moment(values).format('DD/MM/YYYY'));
     };
-
-
 
 
     return (
@@ -192,15 +173,15 @@ const AdminThemKhoaHoc = () => {
                 </Form.Item>
                 <Form.Item label="Mã khóa học">
                     <Input name='maKhoaHoc' onChange={formik.handleChange} />
-                    {formik.errors.maKhoaHoc && formik.touched.maKhoaHoc && (
-                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.maKhoaHoc}</p>
-                    )}
+                    {/* {formik.errors.hoTen && formik.touched.hoTen && (
+                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.hoTen}</p>
+                    )} */}
                 </Form.Item>
                 <Form.Item label="Tên khóa học">
                     <Input name='tenKhoaHoc' onChange={handleChangeTenKH} />
-                    {formik.errors.tenKhoaHoc && formik.touched.tenKhoaHoc && (
-                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.tenKhoaHoc}</p>
-                    )}
+                    {/* {formik.errors.taiKhoan && formik.touched.taiKhoan && (
+                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.taiKhoan}</p>
+                    )} */}
                 </Form.Item>
                 <Form.Item label="Danh mục khóa học:" name='maDanhMucKhoaHoc'>
                     <Select name='maDanhMucKhoaHoc'
@@ -209,19 +190,33 @@ const AdminThemKhoaHoc = () => {
                         onChange={handleChangeKH}
 
                     />
+                    {/* {formik.errors.maLoaiNguoiDung && formik.touched.maLoaiNguoiDung && (
+                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.maLoaiNguoiDung}</p>
+                    )} */}
 
                 </Form.Item>
                 <Form.Item label="Ngày tạo">
                     <DatePicker format='DD/MM/YYYY' onOk={onOk} onChange={onChangeDate} />
-                  
+                    {/* {formik.errors.ngayChieuGioChieu && formik.touched.ngayChieuGioChieu && (
+                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.ngayChieuGioChieu}</p>
+                    )} */}
+
+                    {/* <Input name='ngayTao' onChange={formik.handleChange} /> */}
+                    {/* {formik.errors.matKhau && formik.touched.matKhau && (
+                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.trailer}</p>
+                    )} */}
                 </Form.Item>
                 <Form.Item label="Đánh giá">
                     <Input type='number' name='danhGia' onChange={formik.handleChange} />
-                   
+                    {/* {formik.errors.email && formik.touched.email && (
+                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.email}</p>
+                    )} */}
                 </Form.Item>
                 <Form.Item label="Lượt xem">
                     <Input type='number' name='luotXem' onChange={formik.handleChange} />
-                  
+                    {/* {formik.errors.soDT && formik.touched.soDT && (
+                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.soDT}</p>
+                    )} */}
                 </Form.Item>
                 <Form.Item label="Người tạo:" name='taiKhoanNguoiTao'>
                     <Select name='taiKhoanNguoiTao'
@@ -230,7 +225,9 @@ const AdminThemKhoaHoc = () => {
                         onChange={handleChangeNguoiTao}
 
                     />
-                 
+                    {/* {formik.errors.maLoaiNguoiDung && formik.touched.maLoaiNguoiDung && (
+                        <p className='text-red-500 mb-0 text-sm'>{formik.errors.maLoaiNguoiDung}</p>
+                    )} */}
 
                 </Form.Item>
                 <Form.Item label="Hình Ảnh">
@@ -240,11 +237,12 @@ const AdminThemKhoaHoc = () => {
                             <br />
                             <img style={{ width: 50, height: 50 }} src={imgSrc} alt="." />
                         </div>
-                       
+                    
 
                     </div>
 
 
+                
 
                 </Form.Item>
                 <Form.Item label="Mô tả">
@@ -253,9 +251,12 @@ const AdminThemKhoaHoc = () => {
                 </Form.Item>
 
 
+
+
+
                 <div className='text-center'>
                     <Form.Item label='' >
-                        <Button htmlType="submit" className='bg-blue-600 p-2 text-white rounded-lg hover:bg-blue-900 hover:text-white border hover:border-blue-600'>Thêm Người Dùng</Button>
+                        <Button htmlType="submit" className='bg-blue-600 p-2 text-white rounded-lg hover:bg-blue-900 hover:text-white border hover:border-blue-600'>Thêm Khóa Học</Button>
                     </Form.Item>
                 </div>
 
